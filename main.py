3196d8554f016f181aac09a6c9503d0935db4f61
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-08-21 09:05:21 +0600

    Create PostgreSQL Tables

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 class ChoiceBase(BaseModel):
     choice_text: str
